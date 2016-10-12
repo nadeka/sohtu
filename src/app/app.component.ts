dcf49d7177fd14483c7afb639fd88d03ba7504a6
kmartesu <kim.martesuo@gmail.com>
2016-10-12 17:10:11 +0300

    ng2-bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,7 @@ import { Component, ViewEncapsulation } from '@angular/core';
   selector: 'app',
   encapsulation: ViewEncapsulation.None,
   styles: [ 'app.style.css' ],
+
   templateUrl: 'app.component.html'
 })
 
