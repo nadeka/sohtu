70ce515d4b3c8c070a0adc4aef2f756a088b364f
kmartesu <kim.martesuo@gmail.com>
2016-11-07 15:06:51 +0200

    jasmine UI tests for choosing mailing list

--- src/app/create-campaign/campaign-settings/campaign-mailing-lists/campaign-mailing-lists.spec.ts
+++ src/app/create-campaign/campaign-settings/campaign-mailing-lists/campaign-mailing-lists.spec.ts
@@ -3,6 +3,8 @@ import {
     async
 }
 from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
 
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CampaignMailingLists } from './campaign-mailing-lists.component.ts';
@@ -15,6 +17,21 @@ import { LanguageService } from '../../../services/language.service';
 describe('Component: CampaignMailingLists', () => {
     let fixture: any;
     let component: any;
+    let page: Page;
+
+    class Page {
+      buttons:            Array<DebugElement>;
+      selectAllButton:    DebugElement;
+      deselectAllButton:  DebugElement;
+      selectFirstList:    DebugElement;
+
+      addPageElements() {
+        this.buttons = fixture.debugElement.queryAll(By.css('div'));
+        this.selectAllButton = this.buttons[2];
+        this.deselectAllButton = this.buttons[3];
+        this.selectFirstList = this.buttons[7];
+      }
+    }
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
@@ -33,9 +50,10 @@ describe('Component: CampaignMailingLists', () => {
         }).compileComponents().then(function(arr) {
             fixture = TestBed.createComponent(CampaignMailingLists);
             component = fixture.componentInstance;
-
+            page = new Page();
             // Detect changes to wire up the `fixture.nativeElement` as necessary:
             fixture.detectChanges();
+            page.addPageElements();
         });
     }));
 
@@ -79,8 +97,46 @@ describe('Component: CampaignMailingLists', () => {
         component.mailingLists
             .forEach(mailingList => expect(component.isSelected(mailingList.id)).toBe(false));
     });
+
+    // UI tests
+    it('pressing select all button should select all', () => {
+        // click button and evaluate
+        page.selectAllButton.triggerEventHandler('click', null);
+        component.mailingLists
+            .forEach(mailingList => expect(component.isSelected(mailingList.id)).toBe(true));
+    });
+
+    it('pressing deselect all should deselect all', () => {
+        // click button and evaluate
+        page.selectAllButton.triggerEventHandler('click', null);
+        component.mailingLists
+            .forEach(mailingList => expect(component.isSelected(mailingList.id)).toBe(true));
+
+        // click deselect all and evaluate
+        page.deselectAllButton.triggerEventHandler('click', null);
+        component.mailingLists
+            .forEach(mailingList => expect(component.isSelected(mailingList.id)).toBe(false));
+    });
+
+    it('pressing first list should select it', () => {
+        // select first list
+        page.selectFirstList.triggerEventHandler('click', null);
+        expect(component.getSelected().length).toBe(1);
+    });
+
+    it('first selecting single list and then deselecting it should work', () => {
+        // select first list
+        page.selectFirstList.triggerEventHandler('click', null);
+        expect(component.getSelected().length).toBe(1);
+
+        // click deselect all and evaluate
+        page.deselectAllButton.triggerEventHandler('click', null);
+        component.mailingLists
+            .forEach(mailingList => expect(component.isSelected(mailingList.id)).toBe(false));
+    });
 });
 
+// Helpful functions
 function validateMailingList(mailingList: MailingList) {
     expect(mailingList).toBeDefined();
     expect(mailingList.name).toBeDefined();
--- src/app/services/mailing-lists/mailing-lists.spec.ts
+++ src/app/services/mailing-lists/mailing-lists.spec.ts
@@ -1,4 +1,6 @@
 import { MailingListsService } from './mailing-lists.service';
+import { Contact } from '../../models/contact.model';
+import { MailingList } from '../../models/mailing-list.model';
 
 describe('Service: MailingListsService', () => {
 
@@ -9,6 +11,13 @@ describe('Service: MailingListsService', () => {
     it('should return 3 mailing lists', function() {
         this.mailingListsService.getMailingLists()
             .then(mailingLists => validateMailingLists(mailingLists));
+    });
+
+    it('create mailing list function should work', function() {
+        let c: Contact = new Contact(1, 'firstname', 'lastname', 'test@test.com');
+        let test: MailingList;
+        this.test = this.mailingListsService.createMailingList('test list', 'testing creating list', [this.c]);
+        expect(this.test.members.length).toBe(1);
     });
 });
 
