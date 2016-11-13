41db230eee29a624e9efc9a554a94aa61b888b4e
Otto-Ville Raitolahti <raot@cs.helsinki.fi>
2016-11-13 23:27:58 +0200

    Protractor test for confirmation page

--- src/test/firstTest.e2e.ts
+++
@@ -1,9 +0,0 @@
-describe('New campaign creation page', function() {
-
-    it('should be possible for user to add campaign name and subject', function() {
-        browser.get('http://ext-dev-marketing-automation.s3-website-eu-west-1.amazonaws.com/#/marketing/create-campaign');
-
-        expect(browser.getTitle()).toEqual('Sohtu');
-	});
-
-});
